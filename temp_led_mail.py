5cf30fb83f8e7c742d898d129d03fc891edd4a95
justDabuK <kevinjust87@gmail.com>
2018-03-20 15:23:14 +0000

    added messages

--- temp_led_mail.py
+++ temp_led_mail.py
@@ -6,17 +6,19 @@ from send_email import sendemail
 
 def main():
     sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+    print "Initialized sensor"
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-
+    print "Initialized strip"
     send_flag = 0
-
+    
     while(True):
         degrees = sensor.read_temperature()
-
+        
         if degrees < 30.0:
             colorWipe(strip, Color(0, 0, 255))
             if send_flag == 0:
                 send_flag = 1
+                print "toogle send_flag"
         else:
             colorWipe(strip, Color(255, 0, 0))
             if send_flag == 1:
@@ -29,6 +31,7 @@ def main():
                     'heatsense5000@gmail.com',
                     'TMSBausses'
                 )
+                print "toogle send_flag"
 
 
 if __name__ == "__main__":
