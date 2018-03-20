03ed18f6e27f5e7b6ba5e709023e5b07951790f4
justDabuK <kevinjust87@gmail.com>
2018-03-20 16:24:58 +0100

    added the necessary begin

--- temp_led_mail.py
+++ temp_led_mail.py
@@ -8,12 +8,17 @@ def main():
     sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
     print "Initialized sensor"
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
     print "Initialized strip"
     send_flag = 0
     
     while(True):
         degrees = sensor.read_temperature()
-        
+        degrees = sensor.read_temperature()
+        pascals = sensor.read_pressure()
+        hectopascals = pascals / 100
+        humidity = sensor.read_humidity()
+
         if degrees < 30.0:
             colorWipe(strip, Color(0, 0, 255))
             if send_flag == 0:
