a9f383d5e54a7fb87eaac82a69a4d0eb079df225
justDabuK <kevinjust87@gmail.com>
2018-03-24 14:39:35 +0100

    created the single led test

---
+++ singlePixelTest.py
@@ -0,0 +1,38 @@
+from strandtest import *
+from neopixel import *
+
+def main():
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+
+    try:
+        while True:
+            for i in range(strip.numPixels()):
+                print "LED #", i
+                print "-> red"
+                strip.setPixelColor(i, Color(200, 0, 0, 0))
+                strip.show()
+                time.sleep(1)
+
+                print "-> green"
+                strip.setPixelColor(i, Color(0, 200, 0, 0))
+                strip.show()
+                time.sleep(1)
+
+                print "-> blue"
+                strip.setPixelColor(i, Color(0, 0, 200, 0))
+                strip.show()
+                time.sleep(1)
+
+                print "-> white"
+                strip.setPixelColor(i, Color(0, 0, 0, 200))
+                strip.show()
+                time.sleep(1)
+
+    except KeyboardInterrupt:
+        print "exiting"
+        colorWipe(strip, Color(0, 0, 0), 10)
+
+
+if __name__ == "__main__":
+    main()
