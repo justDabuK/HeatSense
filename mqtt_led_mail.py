4f0b7915501db07fe9f8847df92aa65f06ecd681
justDabuK <kevinjust87@gmail.com>
2018-03-25 16:16:06 +0200

    added a no connection functionality

--- mqtt_led_mail.py
+++ mqtt_led_mail.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt #import the client1
 import time
+import datetime
 from Adafruit_BME280 import *
 from strandtest import *
 from neopixel import *
@@ -7,8 +8,11 @@ from send_email import sendemail
 from traffic_light import *
 
 send_flag = 0
+timestamp = None
+
 
 def on_message(client, userdata, message):
+    timestamp = datetime.datetime.now()
     degrees = float(str(message.payload.decode("utf-8")).split("#")[1])
     #print("message received ", str(degrees))
     #print("message topic=", message.topic)
@@ -18,13 +22,13 @@ def on_message(client, userdata, message):
     global send_flag
 
     if degrees < 28.0:
-        green(strip)
-        if send_flag == 0:
+        if send_flag != 1:
+            green(strip)
             send_flag = 1
             print "everything is ok"
     else:
-        red(strip)
-        if send_flag == 1:
+        if send_flag != 0:
+            red(strip)
             send_flag = 0
             """sendemail(
                 'heatsense5000@gmail.com',
@@ -38,8 +42,10 @@ def on_message(client, userdata, message):
 
 
 if __name__ == "__main__":
+    timestamp = datetime.datetime.now()
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
+    blue(strip)
     print "Initialized strip"
 
 
@@ -58,10 +64,15 @@ if __name__ == "__main__":
 
     try:
         while True:
+            if (datetime.datetime.now() - timestamp).total_seconds() > 5:
+                if send_flag != 2:
+                    send_flag = 2
+                    blue(strip)
             time.sleep(1)
 
-    except KeyboardInterrupt:
+    except:
         print "exiting"
+        colorWipe(strip, Color(0, 0, 0), 0)
         client.disconnect()
         client.loop_stop()
 
